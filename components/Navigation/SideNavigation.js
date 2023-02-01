f1db47918dc063a183608bb165ac3527e9584e12
komutkadum <komutkadum@gmail.com>
2023-02-01 23:35:35 +0530

    feat: added navigation

--- components/Navigation/SideNavigation.js
+++ components/Navigation/SideNavigation.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 
-function SideNavigation({ navOpen, setNavOpen }) {
+function SideNavigation() {
   return (
     <>
       {/* // header with text */}
