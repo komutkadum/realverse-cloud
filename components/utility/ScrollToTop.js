71ec9f433e3257cb4dc1a614b962c3b82d7feca1
komutkadum <komutkadum@gmail.com>
2023-02-21 00:00:39 +0530

    perf: fix accesible links for screen readers

--- components/utility/ScrollToTop.js
+++ components/utility/ScrollToTop.js
@@ -4,6 +4,7 @@ function ScrollToTop() {
   return (
     <a
       href="#top"
+      aria-label="topScroll"
       className="fixed bottom-10 right-10 w-10 h-10 border rounded-full flex justify-center items-center z-50 opacity-80 hover:opacity-100 bg-button"
     >
       <span className="sr-only"></span>
